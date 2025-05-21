c41f9985005dabf89a09fd5483e4811099972949
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2025-05-21 13:46:14 +0530

    for loop

---
+++ myproject/for_loop.py
@@ -0,0 +1,16 @@
+def test():
+
+    i = 10
+
+
+for i in range(3):
+    print(i)
+test()
+
+# for i in range(start, stop, step):
+
+# start: (optional) where to begin (default is 0)
+
+# stop: where to stop (not included)
+
+# step: (optional) how much to increment (default is 1)
---
+++ myproject/for_loop_2.py
@@ -0,0 +1,13 @@
+
+
+for i in range(3, 30, 3):
+    print(i)
+
+
+# for i in range(start, stop, step):
+    # code block
+# start: (optional) where to begin (default is 0)
+
+# stop: where to stop (not included)
+
+# step: (optional) how much to increment (default is 1)

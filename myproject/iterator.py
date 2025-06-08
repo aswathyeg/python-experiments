4e3ebc23a03e7c9887f4826c43a3f8581de2a373
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2025-06-08 17:07:29 +0530

    iterator

---
+++ myproject/iterator.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3]
+iterator = iter(my_list)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+
+#  __iter__() → returns the iterator object itself
+
+#  __next__() → returns the next value
